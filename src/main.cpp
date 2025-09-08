1b8e766281225ee3db1dc40d32f6ef7caecf4204
jessephasouk <jessephasouk@gmail.com>
2025-09-08 15:56:07 -0400

    Final Commit

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,26 @@ int main(){
 	string fullName;
 	string email;
 	
+	// ask for full name
 	std::getline(cin, fullName);
+
+	// ask for email
 	cin >> email;
 
 	string* firstName = new string;
 	string* lastName = new string;
 	
 	StringUtils::parseName(fullName, firstName, lastName);
+	string username = StringUtils::getUsername(email);
+	
+	// print info
+	cout << "First Name: " << *firstName << endl;
+	cout << "Last Name: " << *lastName << endl;
+	cout << "Username: " << username << endl;
+
+	//free memory
+	delete firstName;
+	delete lastName;
+
+	return 0;
 }
