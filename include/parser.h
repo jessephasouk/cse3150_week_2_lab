dd9ad3f16f20cbad8658d12b074e5d3be01de5bb
jessephasouk <jessephasouk@gmail.com>
2025-09-08 14:19:26 -0400

    added files and directories

---
+++ include/parser.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace StringUtils {
+	void parseName(const std::string& fullName, std::string* firstName, std::string* lastName)
+	std::string getUsername(const std::string& email)
+}
---
+++ src/parser.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "parser.h"
+
+void parseName(const std::string& fullName, std::string* firstName, std::string* lastName) {
+	
+}
